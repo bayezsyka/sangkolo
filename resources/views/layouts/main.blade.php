5754dc029c06a95c4057f9db8969b7ca4ec883ec
bayezsyka <farrosy6@gmail.com>
2025-07-25 16:38:16 +0700

    bisalah yok

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,8 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Sangkolo Agensi Jasa</title>
-    <link rel="stylesheet" href="{{ asset('build/assets/app-BQJjDyVl.css') }}">
-    <script type="module" src="{{ asset('build/assets/app-DtCVKgHt.js') }}"></script>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="font-sans antialiased bg-gray-100 text-sangkolo-dark">
     
